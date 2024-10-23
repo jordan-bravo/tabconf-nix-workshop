3e7909d3a0b455e2ad54d7fdb8afeb5e3c9a2dfd
Jordan Bravo <jordan@bravo.cc>
2024-10-23 12:10:18 -0400

    end of workshop

--- flake.nix
+++ flake.nix
@@ -3,7 +3,44 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        defaultPackage = pkgs.rustPlatform.buildRustPackage {
+          pname = "nix-workshop";
+          version = "0.1.0";
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          src = ./.;
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.bitcoind
+          ];
+
+          preBuild = ''
+            mkdir -p $out/data
+            ${pkgs.bitcoind}/bin/bitcoind -regtest -daemon -datadir=$out/data
+          '';
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bitcoind
+            cargo
+            rustc
+          ];
+          shellHook = ''
+            ${pkgs.bitcoind}/bin/bitcoind regtest -daemon
+          '';
+        };
+      });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{
+    io::Read,
+    process::{Command, Stdio},
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Run the bitcoin-cli command
+    let output = Command::new("bitcoin-cli")
+        .arg("--regtest")
+        .arg("getblockchaininfo")
+        .stdout(Stdio::piped()) // Capture the standard output
+        .spawn() // Start the command
+        .expect("Failed to execute bitcoin-cli");
+
+    // Wait for the command to complete and capture the output
+    let mut child_stdout = output.stdout.expect("Failed to capture stdout");
+
+    // Read the output from the process
+    let mut output_string = String::new();
+    child_stdout
+        .read_to_string(&mut output_string)
+        .expect("Failed to read command output");
+
+    // Print the result of bitcoin-cli
+    println!("Output from bitcoin-cli: {}", output_string);
 }
